0d420322b7f30d2106b8ebc875e846c99e8f7690
GFV <guilhermeferreiravieira@outlook.com>
2023-09-07 22:23:55 -0300

    Melhorando testes da classe Device

---
+++ src/device.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import hashlib
+
+class Device:
+    """Classe para criar dispositvos detecdados"""
+
+    # Quantidade de segundos necessários para que um dispositivo seja considerado fora do ônibus
+    TIMEOUT_SECONDS = 300
+    
+    def __init__(self, mac, first_seen):
+        """Cria um objeto a partir do endereço MAC do dispositivo e da primeira vez que o dispositivo foi detectado em uma varredura"""
+        # Usando SHA256 para anonimizar o endereço MAC
+        self.__mac_hash = hashlib.sha256(mac.encode('utf-8')).hexdigest()
+        self.__first_seen = first_seen
+        self.__last_seen = first_seen
+
+
+    def timeout(self):
+        """Verifica se o dispositivo está fora do ônibus"""
+        if(datetime.now() - self.last_seen).total_seconds() > Device.TIMEOUT_SECONDS:
+            return True
+        return False
+    
+    @property
+    def mac_hash(self):
+        """Obtém o hash SHA-256 do endereço MAC do dispositivo"""
+        return self.__mac_hash
+        
+    @property
+    def first_seen(self) -> datetime:
+        """Obtém a primeira vez que o dispositivo foi detectado em uma varredura"""
+        return self.__first_seen
+    
+    @property
+    def last_seen(self) -> datetime:
+        """Obtém a última vez que o dispositivo foi detectado em uma varredura"""
+        return self.__last_seen
+    
+    @last_seen.setter
+    def last_seen(self, new_last_seen : datetime):
+        """Define a última vez que o dispositivo foi detectado em uma varredura"""
+        self.__last_seen = new_last_seen
+    
+    def __str__(self) -> str:
+        """Retorna uma representação em string do objeto"""
+        return f'MAC_HASH: {self.__mac_hash} | First Seen: {self.__first_seen} | Last Seen: {self.__last_seen}'
+    
+    def __eq__(self, other: 'Device') -> bool:
+        """Verifica se os dispositivos são iguais com base nos hashes de seus endereços MAC"""
+        if isinstance(other, Device):
+            return self.__mac_hash == other.__mac_hash
+        return False
---
+++ tests/test_device.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from datetime import timedelta
+import hashlib
+
+from src.device import Device
+
+def test_device_creation():
+    """Testando a criação de um objeto Device"""
+    mac = "00:11:22:33:44:55"
+    first_seen = datetime.now()
+    device = Device(mac, first_seen)
+
+    assert device is not None
+    assert device.mac_hash == hashlib.sha256(mac.encode('utf-8')).hexdigest()
+    assert device.first_seen == first_seen
+    assert device.last_seen == first_seen
+
+def test_device_timeout():
+    """Testando o método timeout"""
+    mac = "00:11:22:33:44:55"
+    first_seen = datetime.now()
+    device = Device(mac, first_seen)
+
+    assert not device.timeout()
+
+    # Defina o last_seen para um tempo no passado para simular expiração
+    device.last_seen = first_seen - timedelta(seconds=Device.TIMEOUT_SECONDS + 1)
+
+    # O dispositivo deve estar expirado agora
+    assert device.timeout()
+
+def test_device_equality():
+    """Testando a igualdade entre dispositivos"""
+    mac1 = "00:11:22:33:44:55"
+    mac2 = "66:77:88:99:AA:BB"
+    first_seen1 = datetime.now()
+    first_seen2 = first_seen1 + timedelta(seconds=10)
+
+    device1 = Device(mac1, first_seen1)
+    device2 = Device(mac2, first_seen2)
+    device3 = Device(mac1, first_seen2)
+
+    assert device1 == device1
+    assert device1 == device3
+    assert device1 != device2
