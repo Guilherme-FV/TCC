7e484b881b2684f2bbb7e93feeb48e493c723a16
GFV <guilhermeferreiravieira@outlook.com>
2023-10-04 21:15:15 -0300

    Organizando a modularização e os imports

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
+from datetime import datetime
 from errno import EEXIST
 from os import makedirs, path
 from multiprocessing import Manager, Process
+from sys import exit
 
-from modules.log_handler import *
-from modules.system_killer import System_Killer
-from modules.bus_device_tracker import get_bus_ocupation, live_device_scanner, position_ping, POSITION_TIMER_SECONDS, OCUPATION_TIMER_SECONDS
+import modules
 
 
 if __name__ == "__main__":
     """Início do programa"""
-    killer = System_Killer()
+    killer = modules.System_Killer()
           
-    if not path.exists(path.dirname(TCPDUMP_LOG)):
+    if not path.exists(path.dirname(modules.TCPDUMP_LOG)):
         try:
-            makedirs(path.dirname(TCPDUMP_LOG))
+            makedirs(path.dirname(modules.TCPDUMP_LOG))
         except OSError as exc:
             if exc.errno != EEXIST:
                 raise
@@ -26,15 +26,21 @@ if __name__ == "__main__":
     position_timer = datetime.now()
     ocupation_timer = datetime.now()
 
-    device_scanner = Process(target=live_device_scanner, args=(enter_devices,))
-    position_tracker = Process(target=position_ping)
-    ocupation_tracker = Process(target=get_bus_ocupation, args=(enter_devices, exit_devices))
+    device_scanner = Process(target=modules.live_device_scanner, args=(enter_devices,))
+    position_tracker = Process(target=modules.position_ping)
+    ocupation_tracker = Process(target=modules.get_bus_ocupation, args=(enter_devices, exit_devices))
 
     device_scanner.start()
     position_tracker.start()
-    position_tracker.start()
-    
+    ocupation_tracker.start()
+
     while True:
         if killer.kill_now:
             print('TERMINANDO O PROGRAMA')
-            raise SystemExit
+            device_scanner.terminate()
+            device_scanner.join()
+            position_tracker.terminate()
+            position_tracker.join()
+            ocupation_tracker.terminate()
+            ocupation_tracker.join()
+            exit(0)
---
+++ src/modules/__init__.py
@@ -0,0 +1,3 @@
+from .bus_device_tracker import get_bus_ocupation, live_device_scanner, position_ping, POSITION_TIMER_SECONDS, OCUPATION_TIMER_SECONDS
+from .log_handler import *
+from .system_killer import System_Killer
