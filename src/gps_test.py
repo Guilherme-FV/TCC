1441c88a6a46835d8beb9466941f02e36c54d594
Guilherme-PC <guilhermeferreiravieira@outlook.com>
2023-10-28 12:10:20 -0300

    Teste

--- src/gps_test.py
+++ src/gps_test.py
@@ -2,5 +2,5 @@ from modules.gps_handler import GPSHandler
 
 gps_handler = GPSHandler()
 
-while True:
-        print(f'Latitude: {gps_handler.latitude}, Longitude: {gps_handler.longitude}, Hora: {gps_handler.timestamp}, Data: {gps_handler.datestamp}')
+# while True:
+#         print(f'Latitude: {gps_handler.latitude}, Longitude: {gps_handler.longitude}, Hora: {gps_handler.timestamp}, Data: {gps_handler.datestamp}')
