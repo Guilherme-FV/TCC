c86c1eb8d0859dbec5001235fb75684fa4a3a2b8
Guilherme-PC <guilhermeferreiravieira@outlook.com>
2023-10-28 17:55:48 -0300

    Novo gps handler

--- src/gps_test.py
+++ src/gps_test.py
@@ -1,7 +1,7 @@
-from time import sleep
-from modules.gps_handler import GPSHandler
+import serial
+import pynmea2
 
-gps_handler = GPSHandler()
+from modules.gps_handler import get_gps_data
+
 while True:
-        print(f'Latitude: {gps_handler.latitude}, Longitude: {gps_handler.longitude}, Timestamp: {gps_handler.timestamp}, Datestamp: {gps_handler.datestamp}')
-        sleep(1)
+        print(get_gps_data())
--- src/modules/gps_handler.py
+++ src/modules/gps_handler.py
@@ -5,50 +5,66 @@ from multiprocessing import Process
 
 class GPSHandler:
 
-    GPS_SERIAL = Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
-
     def __init__(self):
         self.__status = 'V'
         self.__latitude = ''
         self.__longitude = ''
         self.__timestamp = ''
         self.__datestamp = ''
-        self.__gps_process = Process(target=self.update_data)
-        self.gps_process.start()
-
-    def update_data(self):
-        while True:
-            serial_line = self.GPS_SERIAL.readline().decode('latin-1')
-            print(serial_line)
-            if serial_line.startswith('$GPRMC'):
-                gps_data = parse(serial_line)
-                self.__status = gps_data.status
-                if self.status:
-                    self.__latitude = gps_data.latitude
-                    self.__longitude = gps_data.longitude
-                    self.__timestamp = gps_data.timestamp
-                    self.__datestamp = gps_data.datestamp
-
-    @property
-    def gps_process(self) -> Process:
-        return self.__gps_process
 
     @property
     def latitude(self) -> str:
         return self.__latitude
     
+    @latitude.setter
+    def latitude(self, new_latitude):
+        self.__latitude = new_latitude
+    
     @property
     def longitude(self) -> str:
         return self.__longitude
+    
+    @longitude.setter
+    def longitude(self, new_longitude):
+        self.__longitude = new_longitude
     
     @property
     def timestamp(self) -> str:
         return self.__timestamp
     
+    @timestamp.setter
+    def timestamp(self, new_timestamp):
+        self.__timestamp = new_timestamp
+    
     @property
     def datestamp(self) -> str:
         return self.__datestamp
     
+    @datestamp.setter
+    def datestamp(self, new_datestamp):
+        self.__datestamp = new_datestamp
+    
     @property
     def status(self) -> bool:
         return self.__status == 'A'
+    
+    @status.setter
+    def status(self, new_status):
+        self.__status = new_status
+
+    def __str__(self) -> str:
+        return f'Latitude: {self.latitude}, Longitude: {self.longitude}, Timestamp: {self.timestamp}, Datestamp: {self.datestamp}, Status: {self.status}'
+
+def get_gps_data():
+        gps_data = GPSHandler()
+        while True:
+            serial_line = Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
+            if serial_line.startswith('$GPRMC'):
+                parsed_line = parse(serial_line)
+                gps_data.status = parsed_line.status
+                if gps_data.status:
+                    gps_data.latitude = parsed_line.latitude
+                    gps_data.longitude = parsed_line.longitude
+                    gps_data.timestamp = parsed_line.timestamp
+                    gps_data.datestamp = parsed_line.datestamp
+                return gps_data
