9d33cee10e07e21f65c496dcfd03b7719e9cbf62
Guilherme-PC <guilhermeferreiravieira@outlook.com>
2023-10-28 11:41:38 -0300

    Desenvolvimento da classe gps_handler

---
+++ src/modules/gps_handler.py
@@ -0,0 +1,48 @@
+from serial import Serial
+from pynmea2 import parse
+from multiprocessing import Process
+
+
+class GPSHandler:
+
+    GPS_SERIAL = Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
+
+    def __init__(self):
+        self.__gps_process = Process(target=self.update_data)
+        self.gps_process.start()
+
+    def update_data(self):
+        while True:
+            serial_line = self.GPS_SERIAL.readline().decode('utf-8')
+            if serial_line.startswith('$GPRMC'):
+                gps_data = parse(serial_line)
+                self.__status = gps_data.status
+                if self.status:
+                    self.__latitude = gps_data.latitude
+                    self.__longitude = gps_data.longitude
+                    self.__timestamp = gps_data.timestamp
+                    self.__datestamp = gps_data.date
+
+    @property
+    def gps_process(self) -> Process:
+        return self.__gps_process
+
+    @property
+    def latitude(self) -> str:
+        return self.__latitude
+    
+    @property
+    def longitude(self) -> str:
+        return self.__longitude
+    
+    @property
+    def timestamp(self) -> str:
+        return self.__timestamp
+    
+    @property
+    def datestamp(self) -> str:
+        return self.__datestamp
+    
+    @property
+    def status(self) -> bool:
+        return self.__status == 'A'
