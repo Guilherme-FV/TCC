a4127146fe75c46d60bab669dabb645481a0ab73
Guilherme-PC <guilhermeferreiravieira@outlook.com>
2023-10-28 12:05:04 -0300

    Erro besta

--- src/modules/gps_handler.py
+++ src/modules/gps_handler.py
@@ -8,7 +8,7 @@ class GPSHandler:
     GPS_SERIAL = Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
 
     def __init__(self):
-        self.update_data(self)
+        self.update_data()
         self.__gps_process = Process(target=self.update_data)
         self.gps_process.start()
 
